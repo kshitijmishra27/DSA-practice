7e471ca97c284e7741a6079a2e5862a9cdd2425a
Kshitij mishra <kshitijmishra27@gmail.com>
2022-06-22 11:34:44 +0530

    Update 104-maximum-depth-of-binary-tree.cpp

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -17,10 +17,10 @@ public:
             return 0;
         }
         
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
+        int leftheight = maxDepth(root->left);
+        int rightheight = maxDepth(root->right);
         
-        return 1 + max(left, right);
+        return 1 + max(leftheight , rightheight);
         
     }
 };
