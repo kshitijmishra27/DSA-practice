e1e94f3300cd29a3ccdf8f36bb5a2e308cb8a54c
Kshitij mishra <kshitijmishra27@gmail.com>
2022-08-09 10:19:10 +0530

    Time: 41 ms (83.32%), Space: 12.4 MB (55.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -5,11 +5,12 @@ public:
         
         int n = grid.size(), m = grid[0].size();
         
-        if(i<0 || i==n || j<0 || j==m || grid[i][j] == '0'){
+        if(i<0 || i==n || j<0 || j==m || grid[i][j] == '0'){ // if out of bond or grid is water then do nothing
             return;
         }
         
-        grid[i][j] = '0';
+        grid[i][j] = '0'; // after vising it make it 0 inorder to think that it is visited 
+                        // we can also use visited array                    
         
         dfs(i-1, j, grid); // up
         dfs(i+1, j, grid); // down
@@ -28,9 +29,9 @@ public:
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 
-                if(grid[i][j] == '1'){
+                if(grid[i][j] == '1'){ // if it is land then do dfs and count the no. of islands
                    islands++;
-                    dfs(i , j, grid);
+                   dfs(i , j, grid);
                 }
                 
             }
