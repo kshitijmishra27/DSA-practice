d256e416d85e5cebbcf01f9a2f0ae4009dc530df
Kshitij mishra <kshitijmishra27@gmail.com>
2022-08-29 19:46:31 +0530

    Time: 295 ms (5.07%), Space: 16.3 MB (93.25%) - LeetHub

--- 155-min-stack/155-min-stack.cpp
+++ 155-min-stack/155-min-stack.cpp
@@ -1,4 +1,5 @@
 class MinStack {
+    
 public:
     
     vector<int> ans;
@@ -8,20 +9,26 @@ public:
     }
     
     void push(int val) {
+        
         ans.push_back(val);
+       
     }
     
     void pop() {
+      
         ans.pop_back();
+        
     }
     
     int top() {
+        
         return ans.back();
+      
     }
     
     int getMin() {
       
-        return *min_element(ans.begin(), ans.end());
+       return *min_element(ans.begin(), ans.end());
         
     }
 };
