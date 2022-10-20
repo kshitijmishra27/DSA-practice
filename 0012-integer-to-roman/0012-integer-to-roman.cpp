ee81df88225c138048af5f3d755fe3629417158e
Kshitij mishra <kshitijmishra27@gmail.com>
2022-10-20 17:17:59 +0530

    Time: 13 ms (61.30%), Space: 5.8 MB (99.31%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -2,97 +2,21 @@ class Solution {
 public:
     string intToRoman(int num) {
         
-        unordered_map<int,string> mpp;
+        int val[] = {1, 4, 5, 9, 10, 40, 50, 90, 100 , 400, 500, 900, 1000};
+        string str[] = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
         
-        mpp[1] = "I";  mpp[3] = "III"; mpp[4] = "IV"; mpp[5] = "V"; mpp[6] = "VI";
-        mpp[2] = "II";  mpp[7] = "VII"; mpp[8] = "VIII"; mpp[9] = "IX";
-          
-        string ans ;
-                            
-        while(num >= 1000){
-            num-= 1000;
-            ans+="M";
-        }
-                            
-            if(num == 0){
-                return ans;
+        string ans = "";
+        
+        for(int i = 12; i>=0; i--){
+            
+            while(num >= val[i]){
+                num -= val[i];
+                ans += str[i];
             }
-        
-        if(num>=900){
-            num -= 900;
-            ans += "CM";
+            
         }
         
-          if(num == 0){
-                return ans;
-            }
-                            
-        while(num >= 500){
-            num -= 500;
-            ans+= "D";
-        }
-                            
-              if(num == 0){
-                return ans;
-            }
-        
-        while(num >= 400){
-            num -= 400;
-            ans += "CD";
-        }
-        
-          if(num == 0){
-                return ans;
-            }
-                            
-        while( num >= 100){
-            num -= 100;
-            ans+= "C";
-        }
-                            
-           if(num == 0){
-                return ans;
-            }
-        
-        while( num >= 90){
-            num -= 90;
-            ans += "XC";
-        }
-        
-          if(num == 0){
-                return ans;
-            }
-                            
-        while(num >= 50){
-            num -= 50;
-            ans += "L";
-        }
-                            
-              if(num == 0){
-                return ans;
-            }
-        
-        while(num >= 40){
-            num -= 40;
-            ans += "XL";
-        }
-        
-          if(num == 0){
-                return ans;
-            }
-                            
-        while( num >= 10){
-            num -= 10;
-            ans += "X";
-        }
-
-  if(num == 0){
-                return ans;
-            }
-                            
-        ans += mpp[num];
-                            
-                            return ans;
+        return ans;
         
     }
 };
