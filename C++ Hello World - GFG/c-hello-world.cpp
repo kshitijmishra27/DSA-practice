1977df4091925dd3aa9f75f62ef538cddb8bfe46
Kshitij mishra <kshitijmishra27@gmail.com>
2022-10-21 22:30:25 +0530

    Added solution - LeetHub

---
+++ C++ Hello World - GFG/c-hello-world.cpp
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+#include<iostream>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    void helloWorld()
+    {
+    
+        cout << "Hello World";
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    Solution ob;
+    //function call
+    ob.helloWorld();
+    cout<<endl;
+    return 0;
+}
+// } Driver Code Ends
