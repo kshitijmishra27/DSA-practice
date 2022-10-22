e7b673b79429756a21e0fe0b489536b6e0bc2abd
Kshitij mishra <kshitijmishra27@gmail.com>
2022-10-22 23:27:26 +0530

    Added solution - LeetHub

---
+++ Sort a String - GFG/sort-a-string.cpp
@@ -0,0 +1,30 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+string sort(string s);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        cout << sort(s) << endl;
+    }
+return 0;
+}
+
+
+
+
+// } Driver Code Ends
+
+
+string sort(string s){
+    
+    string ans = s;
+    sort(ans.begin(), ans.end());
+    
+    return ans;
+    
+}
